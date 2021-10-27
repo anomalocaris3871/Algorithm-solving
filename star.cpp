4f19f45602d49663e353fa683bc34f6d1300ac5f
Atmosphere <51738923+anomalocaris3871@users.noreply.github.com>
2021-10-27 22:20:28 +0900

    2021-10-27
    print star using recursive call

---
+++ star.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+void printStarRecursive(int i, int j, int n) {
+
+	if ((i / n) % 3 == 1 && (j / n) % 3 == 1) {
+		cout << ' ';
+	}
+	else {
+		if (n / 3 == 0) {
+			cout << "*";
+		}
+		else {
+			printStarRecursive(i, j, n / 3);
+		}
+	}
+
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			printStarRecursive(i, j, n);
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
