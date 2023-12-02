3d977d111be114d4ab59675b7c76b54f72de8fea
lee <anomalocaris3871@gmail.com>
2023-12-02 21:58:01 +0900

    BOJ-11657

---
+++ BOJ-11657.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+long long weights[501];
+
+struct Edge {
+    int start;
+    int end;
+    int weight;
+
+    Edge(int start, int end, int weight) {
+        this->start = start;
+        this->end = end;
+        this->weight = weight;
+    }
+};
+
+int main(void) {
+
+    int vertexNum;
+    int edgeNum;
+    cin >> vertexNum >> edgeNum;
+
+
+    vector<Edge> edges;
+
+    for (int i = 0; i < edgeNum; ++i) {
+        int start;
+        int end;
+        int weight;
+        cin >> start >> end >> weight;
+        edges.emplace_back(start, end, weight);
+    }
+
+    for (int i = 1; i <= vertexNum; ++i) {
+        weights[i] = INT_MAX;
+    }
+
+    weights[1] = 0;
+    bool isCycle = false;
+
+    for (int i = 1; i <= vertexNum; ++i) {
+        for (int j = 0; j < edges.size(); ++j) {
+            int start = edges[j].start;
+            int end = edges[j].end;
+            int weight = edges[j].weight;
+
+            if (weights[start] != INT_MAX && weights[start] + weight < weights[end]) {
+                if (i == vertexNum) {
+                    isCycle = true;
+                    break;
+                }
+                weights[end] = weights[start] + weight;
+            }
+        }
+    }
+
+    if (isCycle) {
+        cout << -1 << endl;
+    } else {
+        for (int i = 2; i <= vertexNum; ++i) {
+            if (weights[i] == INT_MAX) {
+                cout << -1 << endl;
+            } else {
+                cout << weights[i] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
